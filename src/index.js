4ce62aa2072642a36befd2de9e5ce9a22b5d334c
atishenkov <atishenkov@quickq.ru>
2020-04-10 14:02:10 +0300

    billet

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as $ from 'jquery'
+import Post from '@/Post'
+
+// import json from '@/assets/json.json'
+// import xml from '@/assets/data.xml'
+
+import WebpackLogo from '@/assets/icon-square-big.png'
+import './babel'
+
+import '@/styles/style.css'
+import './styles/less.less'
+import './styles/scss.scss'
+
+const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').addClass('code').html(post.toString())
+
+console.log('Post to String:', post.toString())
+
+// console.log('JSON:', json)
+// console.log('XML:', xml)
+
---
+++ webpack.config.js
@@ -0,0 +1,175 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+console.log('IS DEV:', isDev)
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      hmr: isDev,
+      reloadAll: true
+    },
+    },
+    'css-loader'
+  ]
+  if (extra) {
+    loaders.push(extra)
+  }
+  return loaders
+}
+
+const babel_options = presets => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+  if (presets && presets.length > 0) {
+    presets.forEach(preset => {
+      opts.presets.push(preset)
+    });
+  }
+  return opts
+}
+
+const js_loaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: babel_options()
+    }
+  ]
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders
+}
+
+const plugins = () => {
+  const base =  [
+    new HTMLWebpackPlugin({
+      // title: 'Webpack lehius', 
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, './src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist/')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ]
+  if (isProd) {
+    base.push(new BundleAnalyzerPlugin())
+  }
+  return base
+}
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.js'],
+    analytics: './analytics.ts'
+  },
+  output: {
+    filename: filename('.js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev
+  },
+  devtool: isDev ? 'source-map' : '',
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders('less-loader')
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.(png|jpeg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
+      {
+        test: /\.csv$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: js_loaders()
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babel_options(['@babel/preset-typescript'])
+        }
+      }
+    ]
+  }
+}
+
+
+// 2:12:00
